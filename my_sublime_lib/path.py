97683498b5196e6a8dc5037b4d59ad3c3f0e6ce4
FichteFoll <fichtefoll2@googlemail.com>
2014-08-23 19:13:38 +0200

    PEP257 adjustments for my_sublime_lib.path

--- my_sublime_lib/path.py
+++ my_sublime_lib/path.py
@@ -1,3 +1,8 @@
+"""A collection of useful functions related to paths, ST- and non-ST-related.
+
+Also has some ST-specific file extensions as "constants".
+"""
+
 import os
 import re
 import inspect
@@ -5,7 +10,7 @@ from collections import namedtuple
 
 import sublime
 
-__all__ = [
+__all__ = (
     "FTYPE_EXT_KEYMAP",
     "FTYPE_EXT_COMPLETIONS",
     "FTYPE_EXT_SNIPPET",
@@ -19,7 +24,7 @@ __all__ = [
     "file_path_tuple",
     "get_module_path",
     "get_package_name"
-]
+)
 
 
 FTYPE_EXT_KEYMAP        = ".sublime-keymap"
@@ -32,7 +37,9 @@ FTYPE_EXT_TMLANGUAGE    = ".tmLanguage"
 
 
 def root_at_packages(*leafs):
-    """Combines leafs with path to Sublime's Packages folder.
+    """Combine leafs with path to Sublime's Packages folder.
+
+    Requires the API to finish loading on ST3.
     """
     # If we really need to, we dan extract the packages path from sys.path (ST3)
     return os.path.join(sublime.packages_path(), *leafs)
@@ -40,25 +47,25 @@ def root_at_packages(*leafs):
 
 def data_path():
     """Extract Sublime Text's data path from the packages path.
+
     Requires the API to finish loading on ST3.
     """
-    return os.path.split(sublime.packages_path())[0]
+    return os.path.dirname(sublime.packages_path())
 
 
 def root_at_data(*leafs):
-    """Combines leafs with Sublime's ``Data`` folder.
+    """Combine leafs with Sublime's ``Data`` folder.
+
+    Requires the API to finish loading on ST3.
     """
-    data = os.path.join(os.path.dirname(sublime.packages_path()))
-    return os.path.join(data, *leafs)
+    return os.path.join(data_path(), *leafs)
 
 
 FilePath = namedtuple("FilePath", "file_path path file_name base_name ext no_ext")
 
 
 def file_path_tuple(file_path):
-    """Creates a named tuple with the following attributes:
-    file_path, path, file_name, base_name, ext, no_ext
-    """
+    """Create a namedtuple with: file_path, path, file_name, base_name, ext, no_ext."""
     path, file_name = os.path.split(file_path)
     base_name, ext = os.path.splitext(file_name)
     return FilePath(
@@ -72,13 +79,13 @@ def file_path_tuple(file_path):
 
 
 def get_module_path(_file_=None):
-    """Returns a tuple with the normalized module path plus a boolean.
+    """Return a tuple with the normalized module path plus a boolean.
 
     * _file_ (optional)
         The value of `__file__` in your module.
         If omitted, `get_caller_frame()` will be used instead which usually works.
 
-    Returns: (normalized_module_path, archived)
+    Return: (normalized_module_path, archived)
         `normalized_module_path`
             What you usually refer to when using Sublime API, without `.sublime-package`
         `archived`
@@ -99,7 +106,8 @@ def get_module_path(_file_=None):
 
 
 def get_package_path(_file_=None):
-    """Returns the path to the current Sublime Text package.
+    """Get the path to the current Sublime Text package.
+
     Parameters are the same as for `get_module_path`.
     """
     if _file_ is None:
@@ -119,8 +127,7 @@ def get_package_path(_file_=None):
 
 
 def get_package_name(_file_=None):
-    """`return os.path.split(get_package_path(_file_))[1]`
-    """
+    """`return os.path.split(get_package_path(_file_))[1]`."""
     if _file_ is None:
         _file_ = get_caller_frame().f_globals['__file__']
 
@@ -128,7 +135,8 @@ def get_package_name(_file_=None):
 
 
 def get_caller_frame(i=1):
-    """Utilizes the inspect module to get the caller's frame.
+    """Get the caller's frame (utilizing the inspect module).
+
     You can adjust `i` to find the i-th caller, default is 1.
     """
     # We can't use inspect.stack()[1 + i][1] for the file name because ST sets
